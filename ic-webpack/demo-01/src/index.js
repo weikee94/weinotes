bffce15577689faa02b0ac4dfc11e6e2bcfbb3c4
weikee <weikee070194@gmail.com>
2019-06-25 10:55:47 +0800

    update webpack md

---
+++ ic-webpack/demo-01/src/index.js
@@ -0,0 +1,51 @@
+// var avatar = require("./avatar.jpg");
+// console.log(avatar);
+// console.log("webpack init");
+
+// var img = new Image();
+// img.src = avatar;
+// img.classList.add("avatar");
+
+// var root = document.getElementById("root");
+// root.append(img);
+// import "./index.scss"; 这个相当于在全局引入所有的component会被影响
+import style from "./index.scss"; // 模块化打包
+import avatar from "./avatar.jpg";
+import createAvatar from "./createAvatar";
+import number from "./number";
+import "./style.css";
+import "@babel/polyfill";
+
+createAvatar();
+
+var img = new Image();
+img.src = avatar;
+img.classList.add(style.avatar);
+
+var root = document.getElementById("root");
+root.append(img);
+
+var btn = document.createElement("button");
+btn.innerHTML = "add new";
+document.body.appendChild(btn);
+
+btn.onclick = function() {
+  var div = document.createElement("div");
+  div.innerHTML = "item";
+  document.body.appendChild(div);
+};
+
+number();
+
+const arr = [new Promise(() => {}), new Promise(() => {})];
+arr.map(item => {
+  console.log(item);
+});
+
+if (module.hot) {
+  module.hot.accept("./number", () => {
+    document.getElementById("number");
+    document.body.removeChild(document.getElementById("number"));
+    number();
+  });
+}
---
+++ ic-webpack/demo-01/webpack.config.js
@@ -0,0 +1,93 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-eval-source-map",
+  entry: {
+    main: "./src/index.js"
+    // sub: "./src/index.js"
+  },
+  devServer: {
+    contentBase: "./dist",
+    open: true,
+    hot: true,
+    hotOnly: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            // placeholder 占位符
+            name: "[name].[ext]",
+            outputPath: "images/",
+            limit: 2048 // 大过这个size 生成在images folder 里面
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        // 打包顺序-> postcss, sass, css, style 从下往上
+        use: [
+          // style-loader is used for 挂载
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              // 为了解决import scss in scss file,
+              // 如果不加import loaders
+              // 会直接执行 css skip sass and postcss loader
+              importLoaders: 2
+              // 模块化引入css
+              // modules: true
+            }
+          },
+          "sass-loader",
+          // help us add prefix: -m, -webkit, etc ...
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        // 打包顺序-> postcss, sass, css, style 从下往上
+        use: [
+          // style-loader is used for 挂载
+          "style-loader",
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|svg|woff|woff2)$/,
+        use: {
+          loader: "file-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    // 打包之前先清除dist folder
+    new CleanWebpackPlugin(),
+    // html 模版
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    // publicPath: "http://cdn.com",
+    filename: "[name].js", //dest file name
+    path: path.resolve(__dirname, "dist") //dest folder name
+  }
+};
