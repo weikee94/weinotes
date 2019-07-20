51dd35b03b1a85fdf8fca7691b1eaaa12954bd9e
weikee <weikee070194@gmail.com>
2019-07-20 18:01:39 +0800

    update header scss nesting

--- css-practice/css-app/src/App.js
+++ css-practice/css-app/src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <div className="header__title">header here</div>
+      <div className="header__section">
+        <div className="header__section--title">header here</div>
       </div>
     </div>
   );
