ae98a269bbeb301e3a0ac29a07e0284d51d6a1c0
weikee <weikee070194@gmail.com>
2020-04-11 16:35:50 +0800

    setup base css

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,20 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
+
+// define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials/");
 
+// setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
+// setup html file
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
@@ -24,7 +33,9 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    title: "Help title",
     msg: "Help message",
+    name: "Wei",
   });
 });
 
@@ -32,6 +43,22 @@ app.get("/weather", (req, res) => {
   res.send({
     location: "Johor",
     forecast: "Snowing",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("notfound", {
+    title: "Help article not found",
+    msg: "Help article not found",
+    name: "Wei",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("notfound", {
+    title: "Not found page",
+    msg: "Page not found",
+    name: "Wei",
   });
 });
 
