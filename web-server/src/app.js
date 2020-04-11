1e160e84524ce0e2bc5babeaa192bb900cbee9bc
weikee <weikee070194@gmail.com>
2020-04-11 14:58:43 +0800

    add hbs view engine template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,38 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.set("view engine", "hbs");
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-  res.send("hello express");
-});
-
-app.get("/help", (req, res) => {
-  res.send("this is help page");
+  res.render("index", {
+    title: "Weather APP",
+    name: "Wei",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("this is about page");
+  res.render("about", {
+    title: "About APP",
+    name: "Wei",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    msg: "Help message",
+  });
 });
 
 app.get("/weather", (req, res) => {
-  res.send("this is weather page");
+  res.send({
+    location: "Johor",
+    forecast: "Snowing",
+  });
 });
 
 app.listen(3000, () => {
