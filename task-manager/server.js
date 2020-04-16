e7272779a5c1915c20991a71be04ca3cce2c5883
weikee <weikee070194@gmail.com>
2020-04-16 15:31:00 +0800

    init task manager

---
+++ task-manager/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => res.send("API RUNNING"));
+const PORT = 5000;
+
+app.listen(PORT, () => console.log("server running"));
