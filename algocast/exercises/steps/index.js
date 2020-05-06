957919b2e49a56a206223cd8dbe423f45d3f3ff4
weikee <weikee070194@gmail.com>
2020-05-06 18:50:20 +0800

    update with cap and steps iterative

--- algocast/exercises/steps/index.js
+++ algocast/exercises/steps/index.js
@@ -17,6 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  // 想像成matrix
+  // r0 c0 = '#'
+  // r0 c1 = " "
+  // 當c1 number > r1 number 就是空格； 要不然就是 #
+
+  // 長這種樣子
+  // r0c0 r0c1 r0c2
+  // r1c0 r1c1 r1c2
+  // r2c0 r2c1 r2c2
+
+  // 循環row
+  for (let i = 0; i < n; i++) {
+    let stair = "";
+
+    // 循環column
+    for (let j = 0; j < n; j++) {
+      if (j > i) {
+        stair += " ";
+      } else {
+        stair += "#";
+      }
+    }
+
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
