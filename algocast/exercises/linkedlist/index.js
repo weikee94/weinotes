e6ba8c25d255e585f99e2c203e122ffba70cc444
weikee <weikee070194@gmail.com>
2020-05-07 21:30:30 +0800

    second phase study

--- algocast/exercises/linkedlist/index.js
+++ algocast/exercises/linkedlist/index.js
@@ -18,6 +18,91 @@ class LinkedList {
     const newNode = new Node(val, this.head);
     this.head = newNode;
   }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    let node = this.head.next;
+    this.head = node;
+  }
+
+  removeLast() {
+    let node = this.head;
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    while (node) {
+      if (!node.next.next) {
+        node.next = null;
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  insertLast(val) {
+    if (!this.head) {
+      this.head = new Node(val);
+    }
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return (node.next = new Node(val));
+      }
+      node = node.next;
+    }
+  }
+
+  getAt(index) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      node = node.next;
+
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+    }
+    return null;
+  }
 }
 
 module.exports = { Node, LinkedList };
